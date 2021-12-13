00284d9fdce901e6d48ae00dc464aff00200fa09
nikita <nikit0zpart0z@gmail.com>
2021-12-13 21:51:00 +0300

    Added settings for saving media files on prod

--- src/settings/production.py
+++ src/settings/production.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
