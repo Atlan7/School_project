e3b892a5c46732589fa62ae789b88631b106555b
nikita <nikit0zpart0z@gmail.com>
2022-02-07 01:14:19 +0300

    Deleted trash.

--- formulas/urls.py
+++ formulas/urls.py
@@ -6,8 +6,5 @@ from formulas.views import ViewFormulasAboutTheme, ViewThemes
 urlpatterns = [
     path('themes/', ViewThemes.as_view(), name='view-themes'),
     path('themes/theme/<int:theme_pk>', ViewFormulasAboutTheme.as_view(), name='view-formulas'),
-#    path('view-movie/<slug:movie_slug>', ViewMovie.as_view(), name='view-movie'),
-#    path('like-movie/<int:movie_pk>', AddLikeToMovie.as_view(), name='like-movie'),
-#    path('dislike-movie/<int:movie_pk>', AddDislikeToMovie.as_view(), name='dislike-movie'),
 ]
 
